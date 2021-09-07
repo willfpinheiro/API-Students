50a9bffe9c72c0402aa1708feecc4790866e4825
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2021-09-06 22:43:49 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+//Todos os recursos para a api
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    //	Rest endpoins
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Will",
+                        "will@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+}
