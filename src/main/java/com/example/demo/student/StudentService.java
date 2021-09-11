c74f05e6504670d260f9822fec26b22ba4241906
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2021-09-10 23:47:53 -0300

    FEAT router put

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,12 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.beans.Transient;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service // precisamos dizer que aqui tem um componente do tipo service, para poder ser enchergado dentro do controller
@@ -39,5 +42,25 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
 
+    }
+
+    @Transactional //faz com que nao precise usar query
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Student with id"+studentId+"dows not exists"));
+        if (name != null && name.length() > 0 &&
+            !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
